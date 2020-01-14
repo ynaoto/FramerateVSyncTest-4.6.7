ef2a48bacc20d932369f4de5a0098fd9c8c4cf43
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2020-01-14 13:01:42 +0900

    added

---
+++ Assets/FPS.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FPS : MonoBehaviour {
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+		//Application.targetFrameRate = 30;
+		//QualitySettings.vSyncCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = (int)(1/Time.deltaTime) + "fps";
+	}
+}
---
+++ Assets/VSyncCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class VSyncCount : MonoBehaviour {
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "QualitySettings.vSyncCount = " + QualitySettings.vSyncCount;
+	}
+
+	public void Set(int v) {
+		QualitySettings.vSyncCount = v;
+	}
+}
